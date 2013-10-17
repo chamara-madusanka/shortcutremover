73a076858ceeb85047c6dfd756f5fc90fe1345cd
chamara-madusanka <wrchamara89@gmail.com>
2013-10-17 19:43:48 +0530

    Remove Shortcut Virus From Pen drive/Memory Card Simply Without Antivirus

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ShortCutRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbBx1 = new System.Windows.Forms.ComboBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbBx1
+            // 
+            this.cmbBx1.FormattingEnabled = true;
+            this.cmbBx1.Location = new System.Drawing.Point(132, 33);
+            this.cmbBx1.Name = "cmbBx1";
+            this.cmbBx1.Size = new System.Drawing.Size(119, 21);
+            this.cmbBx1.TabIndex = 1;
+            this.cmbBx1.SelectedIndexChanged += new System.EventHandler(this.cmbBx1_SelectedIndexChanged);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(16, 108);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(90, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Recover Now";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(157, 108);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(13, 39);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(113, 15);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "Select the usb drive";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 62);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "This is a simple software to recover your pen drive from the shortcut virus. sele" +
+    "ct the usb drive and simply click \"Recover Now\" button to process. ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 225);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.cmbBx1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ShortCutRemover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBx1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortCutRemover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var drives = from drive in DriveInfo.GetDrives() //search removable drives
+                         where drive.DriveType == DriveType.Removable
+                         select drive;
+            cmbBx1.DataSource = drives.ToList(); //add removable drives to combobox
+        }
+
+        private void cmbBx1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = new Process { StartInfo = startInfo };
+
+            String drive = cmbBx1.Text;
+            char[] arr = drive.ToCharArray();
+            String crctDrv ="";
+
+            for (int i = 0; i < arr.Length-1;i++ )
+            {
+                crctDrv = crctDrv + arr[i];
+            }
+            
+             
+
+            process.Start();
+            process.StandardInput.WriteLine(@crctDrv);
+            process.StandardInput.WriteLine(@"del *.lnk");
+            process.StandardInput.WriteLine(@"attrib -s -r -h *.* /s /d /l");
+            process.StandardInput.WriteLine("exit");
+
+            process.WaitForExit();
+
+            MessageBox.Show("Your pen drive is recovered!");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
